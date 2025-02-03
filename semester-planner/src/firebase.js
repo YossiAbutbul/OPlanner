8418bf284869f801d05a74ef05a3fdc6c6409b00
YossiAbutbul <abyossi22@gmail.com>
2025-02-03 14:22:36 +0200

    Added GitHub Actions for deployment

--- semester-planner/src/firebase.js
+++ semester-planner/src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyCCp3fqZ7tQUMCcZb5LM8zdNv88D3Dnb0M",
-    authDomain: "oplenner-id.firebaseapp.com",
-    projectId: "oplenner-id",
-    storageBucket: "oplenner-id.firebasestorage.app",
-    messagingSenderId: "600772103063",
-    appId: "1:600772103063:web:bc28d7d3c8a2ae670652d2",
-    measurementId: "G-Y0MVW1N7PY"
+    apiKey: process.env.VITE_FIREBASE_API_KEY,
+    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_FIREBASE_APP_ID,
+    measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
