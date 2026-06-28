408f61e939bb467b4109d447186e10e21dc8003a
YossiAbutbul <abyossi22@gmail.com>
2026-06-28 11:33:13 +0300

    chore: remove Desktop reminders announce modal

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import MainContent from "./components/MainContent";
 import RightSidebar from "./components/RightSidebar";
 import Login from "./components/Login";
 import PomodoroPage from "./components/PomodoroPage";
-import ReminderAnnounceModal from "./components/ReminderAnnounceModal";
 
 // Tour pulls in react-joyride (~100KB). One-shot per user, mostly never seen
 // on repeat visits. Lazy-load so it doesn't bloat the initial JS bundle.
@@ -223,7 +222,6 @@ const App: React.FC = () => {
         <Tour run={tour.tourRun} onFinish={tour.finishTour} onSetMobileNav={setMobileNavOpen} />
       </Suspense>
 
-      <ReminderAnnounceModal />
     </div>
   );
 };
